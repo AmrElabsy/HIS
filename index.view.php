777a02036ec590071435628f5c9b2be86277f576
1551999 <55805947+1551999@users.noreply.github.com>
2019-10-26 18:37:09 +0200

    Add files via upload

---
+++ index.view.php
@@ -0,0 +1,236 @@
+<div class="page-header">
+    <img src="public/images/kfs_hos.png">
+</div>
+
+<div class="clinics">
+    <h1 class="text-center"><?php echo lang('clinics'); ?></h1>
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 wow slideInLeft">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-lg-4 wow slideInDown">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-lg-4 wow slideInRight">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-lg-4  wow slideInLeft">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-lg-4 wow slideInUp">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-lg-4 wow slideInRight">
+            <div class="clinic">
+                <div class="clinic-img"><img src="public/images/clinics/teeth.jpg"></div>
+                <div class="clinic-name">Dentist Clinic</div>
+                <div class="clinic-btn text-center">
+                    <a href="#" class="btn btn-outline-success"><?php echo lang('make_appointment') ?></a>
+                    <br/>
+                    <a href="#" class="btn btn-outline-danger">Show Doctors</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="clinic/index" class="show_all text-center">Show More</a>
+</div>
+
+<div class="doctors_home">
+    <h1 class="text-center"><?php echo lang('doctors') ?></h1>
+    <div class="row">
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 wow zoomIn">
+            <div class="doctor">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="doctor-img"><img src="public/images/doctors/doctor.png"></div>
+                    </div>
+
+                    <div class="col-7 text-center">
+                        <div class="doctor-name">
+                            <h4>Dentist Clinic</h4>
+                            <p class="doctor_specialize">Dentist</p>
+                        </div>
+
+                        <div class="doctor-btn">
+                            <a href="#" class="btn btn-success"><i class="fa fa-user"></i> <?php echo lang('view_profile') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="doctor/index" class="show_all text-center">Show More</a>
+</div>
+
+<div class="stats">
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="stat stat-1">
+                <span class="icon"><i class="fas fa-clinic-medical"></i> </span><?php echo lang('clinics') ?>
+                <span class="num" id="num">14</span>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="stat stat-2">
+                <span class="icon"><i class="fa fa-user"></i> </span><?php echo lang('patients') ?>
+                <span class="num" id="num2">150</span>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="stat stat-3">
+                <span class="icon"><i class="fa fa-user-md"></i> </span><?php echo lang('doctors') ?>
+                <span class="num" id="num3">50</span>
+            </div>
+        </div>
+    </div>
+</div>
